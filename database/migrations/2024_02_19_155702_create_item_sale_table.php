fd1ee384bae07e79e2c1b9c34827433ebdcaf83c
Kollawat Rupanya <64556398+zevenfox@users.noreply.github.com>
2024-02-20 10:49:13 +0700

    Add files via upload

---
+++ database/migrations/2024_02_19_155702_create_item_sale_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateItemSaleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('item_sale', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('item_id');
+            $table->unsignedBigInteger('sale_id');
+            $table->integer('quantity');
+            $table->timestamps();
+        
+            $table->foreign('item_id')->references('id')->on('items')->onDelete('cascade');
+            $table->foreign('sale_id')->references('id')->on('sales')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('item_sale');
+    }
+}
+
